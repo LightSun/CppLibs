a7740540015d04079a8a864846c9e66fade46a78
heaven7 <532278976@qq.com>
2021-08-05 19:07:53 +0800

    add stest for lua-ffi

--- support_test/stest.c
+++ support_test/stest.c
@@ -1,4 +1,12 @@
 #ifndef STEST_C
 #define STEST_C
 
+#include <stdio.h>
+
+int stest_parse(int** a, long long val){
+    printf("stest_parse: val = %lld\r\n", val);
+    *(*a) = 1024;
+    return 1;
+}
+
 #endif // STEST_C
