095b95f44e11b63c6bc39173d84fe9ec99cc62d1
heaven7 <532278976@qq.com>
2024-10-22 13:52:12 +0800

    add callgrind_helper.h

---
+++ libs/h7cpp/core/utils/callgrind_helper.h
@@ -0,0 +1,59 @@
+/**
+  callgrind. only used for linux.
+  */
+#ifndef CALLGRIND_HELPER_H
+#define CALLGRIND_HELPER_H
+
+#include <functional>
+#include <string>
+
+#ifdef __linux
+#include "callgrind.h"
+#endif
+
+namespace h7 {
+
+struct CallgrindParams{
+    int number_warmup {0};
+    int repeats {0};
+    void* ctx_warmup {nullptr};
+    void* ctx_mainBody {nullptr};
+    std::function<void()> func_setup;
+    std::function<void(void*,int,int)> func_warmup;
+    std::function<std::string(void*,int,int)> func_mainBody;
+};
+//used for trace callback.
+static inline void run_with_callgrind(const CallgrindParams& ps){
+#ifdef __linux
+    if(ps.func_setup){
+        ps.func_setup();
+    }
+    if(ps.func_warmup){
+        const int warmupCnt = ps.number_warmup != 0 ? ps.number_warmup : 1;
+        for(int i = 0 ; i < warmupCnt; ++i){
+            (void)i;
+            ps.func_warmup(ps.ctx_warmup, i, warmupCnt);
+        }
+    }
+    if(ps.func_mainBody){
+        const int repCnt = ps.repeats != 0 ? ps.repeats : 1;
+        for(int i = 0 ; i < repCnt; ++i){
+            (void)i;
+            CALLGRIND_TOGGLE_COLLECT;
+            auto str = ps.func_mainBody(ps.ctx_mainBody, i, repCnt);
+            CALLGRIND_TOGGLE_COLLECT;
+            if(str.empty()){
+                CALLGRIND_DUMP_STATS;
+            }else{
+                CALLGRIND_DUMP_STATS_AT(str.data());
+            }
+        }
+    }
+#else
+    fprintf(stderr, "only linux support callgrind.\n");
+#endif
+}
+
+}
+
+#endif // CALLGRIND_HELPER_H
